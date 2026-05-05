b850ad8b3b4e40b24b590c0b0d2e518e4dba5f64
Barret Schloerke <barret@posit.co>
2026-05-05 12:55:32 -0400

    feat(shinyreact): port page_bare/page_react/page_react_dep

--- pkg-py/src/shinyreact/__init__.py
+++ pkg-py/src/shinyreact/__init__.py
@@ -1,4 +1,13 @@
 from ._output import ui_output
+from ._page import page_bare, page_react, page_react_dep
 from ._spec import Element, Node, Spec
 
-__all__ = ["Element", "Node", "Spec", "ui_output"]
+__all__ = [
+    "Element",
+    "Node",
+    "Spec",
+    "page_bare",
+    "page_react",
+    "page_react_dep",
+    "ui_output",
+]
