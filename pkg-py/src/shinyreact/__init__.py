05a5e3f5c978343611c8693b823c20754c47e7d5
Barret Schloerke <barret@posit.co>
2026-05-05 12:57:50 -0400

    feat(shinyreact): unified reactive_output decorator

--- pkg-py/src/shinyreact/__init__.py
+++ pkg-py/src/shinyreact/__init__.py
@@ -1,5 +1,6 @@
 from ._output import ui_output
 from ._page import page_bare, page_react, page_react_dep
+from ._reactive_output import reactive_output
 from ._spec import Element, Node, Spec
 
 __all__ = [
@@ -9,5 +10,6 @@ __all__ = [
     "page_bare",
     "page_react",
     "page_react_dep",
+    "reactive_output",
     "ui_output",
 ]
