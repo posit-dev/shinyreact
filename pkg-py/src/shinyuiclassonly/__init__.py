6b0d672f93bd9aff144700a6322c9eb0814d357d
Barret Schloerke <barret@posit.co>
2026-05-19 13:12:52 -0400

    feat(shinyuiclassonly): output_plot

--- pkg-py/src/shinyuiclassonly/__init__.py
+++ pkg-py/src/shinyuiclassonly/__init__.py
@@ -19,6 +19,7 @@ from ._input_action_button import input_action_button
 from ._input_select import input_select
 from ._input_slider import input_slider
 from ._output_code import output_code
+from ._output_plot import output_plot
 from ._roles import UiInput, UiLayout, UiOutput
 
 __all__ = [
@@ -35,4 +36,5 @@ __all__ = [
     "input_select",
     "input_slider",
     "output_code",
+    "output_plot",
 ]
